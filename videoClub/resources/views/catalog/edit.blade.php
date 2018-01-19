141da6c4903d0043b10380cb4af1f41eb960202c
dianikt <druizpena@iesesteveterradas.com>
2018-01-19 17:36:26 +0100

    ejercicio practica

---
+++ videoClub/resources/views/catalog/edit.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+   Pagina para editar 
+
+@stop
---
+++ videoClub/resources/views/catalog/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+    Pantalla para mostrar las peliculas
+
+@stop
